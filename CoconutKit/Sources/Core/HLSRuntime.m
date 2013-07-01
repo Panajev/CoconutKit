400db04fef11a2ab1a93cc4714bd4dad07881f81
Samuel Défago <defagos@gmail.com>
2013-07-01 06:43:38 +0200

    Fix method swizzling in class hierarchies

--- CoconutKit/Sources/Core/HLSRuntime.m
+++ CoconutKit/Sources/Core/HLSRuntime.m
@@ -8,6 +8,8 @@
 
 #import "HLSRuntime.h"
 
+#import <objc/message.h>
+
 IMP HLSSwizzleClassSelector(Class clazz, SEL selector, IMP newImplementation)
 {
     // Get the original implementation we are replacing
@@ -24,15 +26,45 @@ IMP HLSSwizzleClassSelector(Class clazz, SEL selector, IMP newImplementation)
 
 IMP HLSSwizzleSelector(Class clazz, SEL selector, IMP newImplementation)
 {
-    // Get the original implementation we are replacing
-    Method method = class_getInstanceMethod(clazz, selector);
-    IMP origImp = method_getImplementation(method);
-    if (! origImp) {
+    Method origMethodOnClass = NULL;
+    
+    unsigned int numberOfMethods = 0;
+    Method *methods = class_copyMethodList(clazz, &numberOfMethods);
+    for (unsigned int i = 0; i < numberOfMethods; ++i) {
+        Method method = methods[i];
+        if (method_getName(method) == selector) {
+            origMethodOnClass = method;
+            break;
+        }
+    }
+    free(methods);
+    
+    if (! origMethodOnClass) {
+        Method origMethodOnParentClass = class_getInstanceMethod(clazz, selector);
+        if (! origMethodOnParentClass) {
+            return NULL;
+        }
+        // No SEL argument
+        class_addMethod(clazz, selector, imp_implementationWithBlock(^(id self, va_list argp) {
+            struct objc_super super;
+            super.receiver = self;
+            super.super_class = class_getSuperclass(clazz);
+            return objc_msgSendSuper(&super, selector, argp);
+        }), method_getTypeEncoding(origMethodOnParentClass));
+        
+        origMethodOnClass = class_getInstanceMethod(clazz, selector);
+        if (! origMethodOnClass) {
+            return NULL;
+        }
+    }
+    
+    IMP origImpOnClass = method_getImplementation(origMethodOnClass);
+    if (! origImpOnClass) {
         return NULL;
     }
     
-    class_replaceMethod(clazz, selector, newImplementation, method_getTypeEncoding(method));
-    return origImp;
+    class_replaceMethod(clazz, selector, newImplementation, method_getTypeEncoding(origMethodOnClass));
+    return origImpOnClass;
 }
 
 BOOL HLSIsSubclassOfClass(Class subclass, Class superclass)
